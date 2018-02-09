9d4c88a33d880fa2d38b9a2d821e859a3de7a3ef
Luis Santiago <luis.santiagoayala@gmail.com>
2018-02-08 22:34:20 -0800

    Load shelf options

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,6 @@ class Book extends Component {
 
   render() {
     const { books } = this.props
-    let imageUrl
 
     return (
       <ol className="books-grid">
@@ -18,7 +17,7 @@ class Book extends Component {
             <div className="book">
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
-                <BookshelfChanger />
+                <BookshelfChanger book={book} shelf={book.shelf} />
               </div>
               <div className="book-title">{book.title}</div>
               <div className="book-authors">{book.authors.map((author) => author + " ")}  </div>
--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
 
 class BookshelfChanger extends Component {
+  static propTypes = {
+    shelf: PropTypes.string.isRequired,
+    book: PropTypes.object.isRequired
+  }
+
+  state = {
+    shelfState: ""
+  }
+
+  updateShelf(book, shelf) {
+    BooksAPI.update(book, shelf).then(book => {
+      console.log(book);
+    })
+  }
+
   render() {
+    const { shelf, book } = this.props
+
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select value={shelf}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
